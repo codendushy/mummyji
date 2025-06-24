a133fdc5fb2d860a6d713363cad7ddaafbd7b9f6
codendushy <158252323+codendushy@users.noreply.github.com>
2025-06-24 23:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pickle
 MODEL_PATH = 'model3.h5'
 LABEL_ENCODER_PATH = 'label_encoder.pkl'
 
-@st.cache_resource(show_spinner=False, persist="clear")
+@st.cache_resource(show_spinner=False)
 def load_artifacts():
     model = load_model(MODEL_PATH, compile=False)
     with open(LABEL_ENCODER_PATH, 'rb') as f:
